93ad36faa88406ebf59eba86c281ea15fead35eb
Chester <374488168@qq.com>
2020-06-27 17:56:24 +0800

    Add Microservice Demo and use Consul to manage services

---
+++ CSharper_Core/MicroService01/Controllers/HealthController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Consul;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace MicroService01.Controllers
+{
+[Route("api/[controller]")]
+[ApiController]
+public class HealthController : ControllerBase
+{
+    private readonly ILogger _logger;
+
+    public HealthController(ILogger<HealthController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Get()
+    {
+        _logger.LogInformation($"This is Health Check");
+        return Ok();
+    }
+}
+}
