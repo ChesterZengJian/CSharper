b282e86b6a6efb192bd52eb47499cac2319699fd
Chester <374488168@qq.com>
2020-06-28 01:45:05 +0800

    Add RabbitMQ Demo that simple producer-consumer

---
+++ CSharper_Core/RabbitmqConsumer/Program.cs
@@ -0,0 +1,35 @@
+﻿using RabbitMQ.Client;
+using System;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+namespace RabbitmqConsumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var connectionFactory = new ConnectionFactory
+            {
+                UserName = "chester",
+                Password = "123456",
+                HostName = "192.168.3.125"
+            };
+
+            var connection = connectionFactory.CreateConnection();
+            var channel = connection.CreateModel();
+            var eventConsumer = new EventingBasicConsumer(channel);
+            eventConsumer.Received += (ch, es) =>
+            {
+                var msg = Encoding.UTF8.GetString(es.Body.ToArray());
+                Console.WriteLine($"Receive the message: {msg}");
+                channel.BasicAck(es.DeliveryTag, false);
+            };
+            channel.BasicConsume("hello", false, eventConsumer);
+            Console.WriteLine("Consumer has started");
+            Console.Read();
+            channel.Dispose();
+            connection.Dispose();
+        }
+    }
+}
---
+++ CSharper_Core/RabbitmqProducer/Program.cs
@@ -0,0 +1,34 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+
+namespace RabbitmqProducer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var connectionFacotory = new ConnectionFactory
+            {
+                UserName = "chester",
+                Password = "123456",
+                HostName = "192.168.3.125"
+            };
+            var connection = connectionFacotory.CreateConnection();
+            var channel = connection.CreateModel();
+            channel.QueueDeclare("hello", false, false, false, null);
+            Console.WriteLine("\nRabbitMQ connection successfully");
+
+            string input;
+            do
+            {
+                input = Console.ReadLine();
+                var sendBytes = Encoding.UTF8.GetBytes(input);
+                channel.BasicPublish("", "hello", null, sendBytes);
+            } while (!input.Trim().ToLower().Equals("exit"));
+
+            channel.Close();
+            connection.Close();
+        }
+    }
+}
