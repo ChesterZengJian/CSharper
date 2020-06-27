bc73d92a403f1bd70f9d9f70bc2de32e276827cc
Chester <374488168@qq.com>
2020-06-27 17:50:50 +0800

    添加 LoggerDemo，使用默认 Logger Provider

---
+++ CSharper_Core/LoggerDemo/Controllers/AccountsController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace LoggerDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountsController : ControllerBase
+    {
+        private readonly ILogger m_logger;
+
+        public AccountsController(ILogger<AccountsController> logger)
+        {
+            m_logger = logger;
+        }
+
+        public string Get()
+        {
+            m_logger.LogInformation($"Log Info: This is the {nameof(AccountsController)}");
+            return nameof(AccountsController);
+        }
+    }
+}
