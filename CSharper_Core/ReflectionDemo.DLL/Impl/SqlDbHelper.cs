14d318dd28302f5ab375496ef98577ebdb93350c
Chester <374488168@qq.com>
2020-07-12 23:34:09 +0800

    Add Reflecction Demo

---
+++ CSharper_Core/ReflectionDemo.DLL/Impl/SqlDbHelper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ReflectionDemo.DLL.Impl
+{
+    public class SqlDbHelper : IDBHelper
+    {
+        public void Show()
+        {
+            Console.WriteLine($"{nameof(Show)}");
+        }
+
+        public void Get()
+        {
+            //Console.WriteLine($"{nameof(Get)}");
+        }
+    }
+}
---
+++ CSharper_Core/ReflectionDemo/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Diagnostics;
+using System.Globalization;
+using System.IO;
+using System.Reflection;
+using ReflectionDemo.DLL.Impl;
+
+namespace ReflectionDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //{
+            //    var assemblyName = $"{nameof(ReflectionDemo)}.{nameof(DLL)}";
+            //    var typeName =
+            //        $"{nameof(ReflectionDemo)}.{nameof(DLL)}.{nameof(DLL.Impl)}.{nameof(DLL.Impl.SqlDbHelper)}";
+            //    var assembly = Assembly.Load(assemblyName);
+            //    //var assembly = Assembly.LoadFrom($"{assemblyName}.dll");
+            //    //var assembly = Assembly.LoadFrom(Path.Combine(AppContext.BaseDirectory, $"{assemblyName}.dll"));
+            //    //var assembly = Assembly.LoadFile(Path.Combine(AppContext.BaseDirectory, $"{assemblyName}.dll"));
+
+            //    Console.WriteLine("Types:");
+            //    foreach (var type in assembly.GetTypes())
+            //    {
+            //        Console.WriteLine(type.Name);
+            //    }
+
+            //    Console.WriteLine("\nCalled Method:");
+            //    var sqlDbHelper = assembly.GetType(typeName);
+            //    dynamic sqlHelper = Activator.CreateInstance(sqlDbHelper);
+            //    sqlHelper.Show();
+            //}
+
+            {
+                Monitor();
+            }
+        }
+
+        static void Monitor()
+        {
+            const int loop = 100_000_000;
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+            for (int i = 0; i < loop; i++)
+            {
+                var sqlHelper = new SqlDbHelper();
+                sqlHelper.Get();
+            }
+
+            stopwatch.Stop();
+            Console.WriteLine($"New class:{stopwatch.ElapsedMilliseconds}");
+
+            stopwatch.Reset();
+            stopwatch.Start();
+            var assembly = Assembly.Load($"{nameof(ReflectionDemo)}.{nameof(DLL)}");
+            var type = assembly.GetType(
+                $"{nameof(ReflectionDemo)}.{nameof(DLL)}.{nameof(DLL.Impl)}.{nameof(DLL.Impl.SqlDbHelper)}");
+            for (int i = 0; i < loop; i++)
+            {
+                dynamic sqlHelperReflection = Activator.CreateInstance(type);
+                sqlHelperReflection.Get();
+            }
+
+            stopwatch.Stop();
+            Console.WriteLine($"Reflection class:{stopwatch.ElapsedMilliseconds}");
+        }
+    }
+}
