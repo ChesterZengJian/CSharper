5c2674f86b9af6185bf36b3c2ffa3cd3697a5d9d
Chester <374488168@qq.com>
2020-07-30 00:18:25 +0800

    Add IO and serialize

---
+++ CSharper_Core/IOSerializeDemo/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Configuration;
+using System.IO;
+using System.Text;
+
+namespace IOSerializeDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                //var logPath = ConfigurationManager.AppSettings["LogPath"].ToString();
+
+                //Console.WriteLine(Path.Combine(@"F:\Dockers", "info.exe"));
+
+                var filePath = Path.Combine(@"F:\Dockers\test1", "text.txt");
+
+                //using var fileStream = File.Create(Path.Combine(@"F:\Dockers\test1", "text.txt"));
+                //var name = "231423sdfafa";
+                //var bytes = Encoding.Default.GetBytes(name);
+                //fileStream.Write(bytes, 0, bytes.Length);
+                //fileStream.Flush();
+
+                //using var fileStream = File.Create(Path.Combine(@"F:\Dockers\test1", "text.txt"));
+                //var name = "231423sdfafa";
+                //var sw = new StreamWriter(fileStream);
+                //sw.WriteLine(name);
+                //sw.Flush();
+
+                //using var sw = File.AppendText(filePath);
+                //var msg = "This is Append text";
+                //sw.WriteLine(msg);
+                //sw.Flush();
+
+                //foreach (var line in File.ReadAllLines(filePath))
+                //{
+                //    Console.WriteLine(line);
+                //}
+
+                //Console.WriteLine(File.ReadAllText(filePath));
+
+                //var bytes = File.ReadAllBytes(filePath);
+                //Console.WriteLine(Encoding.Default.GetString(bytes));
+
+                //using var fs = File.OpenRead(filePath);
+                //const int length = 5;
+                //var result = 0;
+
+                //do
+                //{
+                //    var bytes = new byte[length];
+                //    result = fs.Read(bytes, 0, 5);
+                //    for (var i = 0; i < result; i++)
+                //    {
+                //        Console.WriteLine(bytes[i].ToString());
+                //    }
+                //} while (length == result);
+
+                //File.Move(filePath,destFileName)
+                //File.Copy(filePath, destFileName);
+                //File.Delete(filePath);
+            }
+
+            {
+                //if (Directory.Exists(@"F:\Dockers"))
+                //{
+                //    Console.WriteLine("Directory exists");
+                //}
+
+                //var directoryInfo = new DirectoryInfo(@"F:\Dockerss");
+                //Console.WriteLine(directoryInfo);
+
+                //Directory.CreateDirectory(@"F:\Dockers\test1\test2.txt");
+
+                //Directory.Move(@"F:\Dockers\test1", @"F:\Dockers");
+
+                //Directory.Delete(@"F:\Dockers\test2");
+
+                //Console.WriteLine(Path.GetDirectoryName(@"D:\dr"));
+                //Console.WriteLine(Path.GetDirectoryName(@"D:\dr\"));
+
+                //Console.WriteLine(Path.GetRandomFileName());
+                //Console.WriteLine(Path.GetFileNameWithoutExtension(@"D:\dr.txt"));
+
+                Console.WriteLine(Path.GetInvalidFileNameChars());
+                Console.WriteLine(Path.GetInvalidPathChars());
+            }
+        }
+    }
+}
