2201779217421d1b5052d909b1b5830334c980f2
Chester <374488168@qq.com>
2020-08-05 01:02:06 +0800

    Add Stack Exchange Redis

---
+++ CSharper_Core/StackExchangeRedisDemo/Extensions/RedisConnectionHelper.cs
@@ -0,0 +1,116 @@
+﻿using StackExchange.Redis;
+
+namespace StackExchangeRedisDemo.Extensions
+{
+    public class RedisConnectionHelper
+    {
+        private const string RedisConnectionString = "127.0.0.1:6379"; //"192.168.8.73,password=111111,connectTimeout=10000,abortConnect=false";// ConfigurationManager.AppSettings["RedisExchangeHosts"];
+        private static readonly object Locker = new object();
+        private static ConnectionMultiplexer _instance;
+
+        /// <summary>
+        /// 单例获取
+        /// </summary>
+        public static ConnectionMultiplexer Instance
+        {
+            get
+            {
+                if (_instance == null || !_instance.IsConnected)
+                {
+                    lock (Locker)
+                    {
+                        _instance = GetManager();
+                    }
+                }
+                else if (_instance != null)
+                {
+                    //LogHelper.WriteToLog("获取连接 IsConnected：" + _instance.IsConnected, "RedisConnectionHelp");
+                }
+                return _instance;
+            }
+        }
+
+        private static ConnectionMultiplexer GetManager(string connectionString = null)
+        {
+            connectionString ??= RedisConnectionString;
+            var connect = ConnectionMultiplexer.Connect(connectionString);
+
+            //LogHelper.WriteToLog("获取连接：" + connectionString, "RedisConnectionHelp");
+
+            //注册如下事件
+            connect.ConnectionFailed += MuxerConnectionFailed;
+            connect.ConnectionRestored += MuxerConnectionRestored;
+            connect.ErrorMessage += MuxerErrorMessage;
+            connect.ConfigurationChanged += MuxerConfigurationChanged;
+            connect.HashSlotMoved += MuxerHashSlotMoved;
+            connect.InternalError += MuxerInternalError;
+
+            return connect;
+        }
+
+        #region 事件
+
+        /// <summary>
+        /// 配置更改时
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private static void MuxerConfigurationChanged(object sender, EndPointEventArgs e)
+        {
+            //LogHelper.WriteToLog("Configuration changed: " + e.EndPoint, "RedisConnectionHelp");
+        }
+
+        /// <summary>
+        /// 发生错误时
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private static void MuxerErrorMessage(object sender, RedisErrorEventArgs e)
+        {
+            //LogHelper.WriteToLog("ErrorMessage: " + e.Message, "RedisConnectionHelp");
+        }
+
+        /// <summary>
+        /// 重新建立连接之前的错误
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private static void MuxerConnectionRestored(object sender, ConnectionFailedEventArgs e)
+        {
+            //LogHelper.WriteToLog("ConnectionRestored: " + e.EndPoint, "RedisConnectionHelp");
+        }
+
+        /// <summary>
+        /// 连接失败 ， 如果重新连接成功你将不会收到这个通知
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private static void MuxerConnectionFailed(object sender, ConnectionFailedEventArgs e)
+        {
+            //LogHelper.WriteToLog("重新连接：Endpoint failed: " + e.EndPoint + ", " + e.FailureType + (e.Exception == null ? "" : (", " + e.Exception.Message)), "RedisConnectionHelp");
+        }
+
+        /// <summary>
+        /// 更改集群
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private static void MuxerHashSlotMoved(object sender, HashSlotMovedEventArgs e)
+        {
+            //LogHelper.WriteToLog("HashSlotMoved:NewEndPoint" + e.NewEndPoint + ", OldEndPoint" + e.OldEndPoint, "RedisConnectionHelp");
+        }
+
+        /// <summary>
+        /// redis类库错误
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private static void MuxerInternalError(object sender, InternalErrorEventArgs e)
+        {
+            //LogHelper.WriteToLog("InternalError:Message" + e.Exception.Message, "RedisConnectionHelp");
+        }
+
+        #endregion 事件
+
+    }
+}
---
+++ CSharper_Core/StackExchangeRedisDemo/Repositories/RedisHandler.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using StackExchange.Redis;
+using StackExchangeRedisDemo.Extensions;
+
+namespace StackExchangeRedisDemo.Repositories
+{
+    public class RedisHandler
+    {
+        private IDatabase m_database;
+        private readonly int m_dbId;
+        private RedisHandler(int dbId = 0)
+        {
+            m_dbId = dbId;
+        }
+
+        public IDatabase DataBase => GetRedisConn().GetDatabase(m_dbId);
+
+
+        private static RedisHandler _instance;
+
+        /// <summary>
+        /// 实例列表，每个redis的每个db建立一个Instance
+        /// </summary>
+        private static Dictionary<int, RedisHandler> _insList;
+
+        /// <summary>
+        /// 单例访问
+        /// </summary>
+        public static RedisHandler Instance
+        {
+            get
+            {
+                if (null == _instance)
+                    _instance = new RedisHandler();
+                if (null == _insList)
+                    _insList = new Dictionary<int, RedisHandler> { [0] = _instance };
+                return _instance;
+            }
+        }
+
+        public RedisHandler this[int dbId]
+        {
+            get
+            {
+                if (dbId > 255 || dbId < 0)
+                    throw new ArgumentOutOfRangeException(nameof(dbId), @"must be integer between 0 to 255");
+                if (null == _insList)
+                    _insList = new Dictionary<int, RedisHandler>();
+
+                if (!_insList.ContainsKey(dbId))
+                    _insList.Add(dbId, new RedisHandler(dbId));
+
+                var ins = _insList[dbId];
+                return ins;
+            }
+        }
+
+        public ConnectionMultiplexer GetRedisConn()
+        {
+            return RedisConnectionHelper.Instance;
+        }
+
+        public int RetryTimes { get; set; } = 1;
+
+
+        #region 字符串操作
+
+        /// <summary>
+        /// 获取字符串类型的redis值
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        public RedisValue StringGet(RedisKey key)
+        {
+            var errCount = 0;
+            redo:
+            try
+            {
+                return DataBase.StringGet(key);
+            }
+            catch (Exception)
+            {
+                if (errCount >= RetryTimes) throw;
+                errCount++;
+                goto redo;
+            }
+        }
+
+
+        /// <summary>
+        /// 添加带无生命周期的数据
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public bool StringSet(RedisKey key, RedisValue value)
+        {
+            var errCount = 0;
+            redo:
+            try
+            {
+                var ret = DataBase.StringSet(key, value);
+                return ret;
+            }
+            catch
+            {
+                if (errCount >= RetryTimes) throw;
+                errCount++;
+                goto redo;
+            }
+        }
+
+        #endregion
+
+        #region 哈希值操作
+
+        public bool HashSet(RedisKey key, RedisValue hashField, RedisValue value)
+        {
+            var errCount = 0;
+            redo:
+            try
+            {
+                return DataBase.HashSet(key, hashField, value);
+            }
+            catch (Exception)
+            {
+                if (errCount >= RetryTimes) throw;
+                errCount++;
+                goto redo;
+            }
+        }
+
+        public RedisValue[] HashValues(RedisKey key)
+        {
+            var errCount = 0;
+            redo:
+            try
+            {
+                return DataBase.HashValues(key);
+            }
+            catch (Exception)
+            {
+                if (errCount >= RetryTimes) throw;
+                errCount++;
+                goto redo;
+            }
+        }
+
+        #endregion
+    }
+}
