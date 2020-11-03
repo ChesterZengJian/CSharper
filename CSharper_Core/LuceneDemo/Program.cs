57fa8d790fdebf0d05eebdaae9278331b7996bd3
chester <374488168@qq.com>
2020-11-03 22:46:11 +0800

    Init LuceneDemo project

---
+++ CSharper_Core/LuceneDemo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LuceneDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
