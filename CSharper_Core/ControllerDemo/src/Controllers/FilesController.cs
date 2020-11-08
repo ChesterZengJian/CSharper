8fdc66e753326062e9d40dc175d67da7aeff266f
chester <374488168@qq.com>
2020-11-08 17:01:43 +0800

    Add ControllerDemo project and test return file

---
+++ CSharper_Core/ControllerDemo/src/Controllers/FilesController.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.IO;
+
+namespace ControllerDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FilesController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult> Get()
+        {
+            var file = new FileInfo(@"F:\My_GitHubProject\CSharper\CSharper_Core\ControllerDemo\src\Files\File01.txt");
+            var fileStream = await Task.Run(() => file.OpenRead());
+
+            return File(fileStream, "application/octet-stream");
+        }
+    }
+}
