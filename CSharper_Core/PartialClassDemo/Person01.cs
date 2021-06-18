57eacf0f5570da38d83390c95cf34232ff50ae4d
chester <374488168@qq.com>
2021-06-18 23:02:29 +0800

    Add partial class demo

---
+++ CSharper_Core/PartialClassDemo/Person01.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PartialClassDemo
+{
+    public partial class Person
+    {
+        public void SayHello()
+        {
+            Console.WriteLine(m_name);
+        }
+    }
+}
---
+++ CSharper_Core/PartialClassDemo/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace PartialClassDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var person=new Person("Chester");
+            person.SayHello();
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    public partial class Person
+    {
+        private string m_name;
+
+        public Person(string name)
+        {
+            m_name = name;
+        }
+    }
+
+
+}
