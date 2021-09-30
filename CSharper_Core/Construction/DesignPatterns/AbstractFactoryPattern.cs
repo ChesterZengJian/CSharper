0cf2244001e4f09f3ae3ae1ab43dba18aaf2e3ac
Chester <374488168@qq.com>
2021-10-01 04:05:25 +0800

    Add Dynamic Factory and Abstract Factory

---
+++ CSharper_Core/Construction/DesignPatterns/AbstractFactoryPattern.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Construction.DesignPatterns
+{
+    //class MainClass
+    //{
+    //    static void Main(string[] args)
+    //    {
+    //        PartsFactory myfactory = null;
+    //        InventoryMgr myinvmgr = new InventoryMgr();
+    //        foreach (string marg in args)
+    //        {
+    //            switch (marg)
+    //            {
+    //                case "Monitors": myfactory = new MonitorInvFactory(); break;
+    //                case "Keyboards": myfactory = new KeyboardInvFactory(); break;
+    //                default: break;
+    //            }
+    //            if (myfactory != null) myinvmgr.ReplenishInventory(myfactory); myfactory = null;
+    //        }
+    //    }
+    //}
+    //class InventoryMgr { public void ReplenishInventory(PartsFactory vfactory) { IPartsInventory PartInv = vfactory.ReturnPartInventory(); PartInv.Restock(); } }
+    //interface IPartsInventory { void Restock(); }
+    //class MonitorInventory : IPartsInventory { public void Restock() { Console.WriteLine("The monitor inventory has been restocked"); } }
+    //class KeyboardInventory : IPartsInventory { public void Restock() { Console.WriteLine("The keyboard inventory has been restocked"); } }
+    //abstract class PartsFactory { public abstract IPartsInventory ReturnPartInventory(); }
+    //class MonitorInvFactory : PartsFactory { public override IPartsInventory ReturnPartInventory() { return (IPartsInventory)new MonitorInventory(); } }
+    //class KeyboardInvFactory : PartsFactory { public override IPartsInventory ReturnPartInventory() { return (IPartsInventory)new KeyboardInventory(); } }
+
+
+
+    class MainClass
+    {
+        static void Main(string[] args)
+        {
+            InventoryMgr InvMgr = new InventoryMgr();
+            foreach (string marg in args)
+            {
+                switch (marg)
+                {
+                    case "Monitors":
+                        InvMgr.ReplenishInventory(enmInvParts.Monitors);
+                        break;
+                    case "Keyboards": InvMgr.ReplenishInventory(enmInvParts.Keyboards); break;
+                    default: break;
+                }
+            }
+        }
+    }
+    public enum enmInvParts : int { Monitors = 1, Keyboards };
+    class InventoryMgr { public void ReplenishInventory(enmInvParts InventoryPart) { PartsFactory factory = new PartsFactory(); IPartsInventory IP = factory.ReturnPartInventory(InventoryPart); IP.Restock(); } }
+    //class PartsFactory { public IPartsInventory ReturnPartInventory(enmInvParts InvPart) { IPartsInventory InvType; switch (InvPart) { case enmInvParts.Monitors: InvType = new MonitorInventory(); break; case enmInvParts.Keyboards: InvType = new KeyboardInventory(); break; default: InvType = null; break; } return InvType; } }
+}
---
+++ CSharper_Core/Construction/DesignPatterns/DynamicFactoryPattern.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Construction.DesignPatterns
+{
+    [AttributeUsage(AttributeTargets.Class)]
+    public class InventoryPartAttribute : Attribute
+    {
+        private enmInvParts mInventoryPart;
+        public InventoryPartAttribute(enmInvParts vInvPart) { mInventoryPart = vInvPart; }
+        public enmInvParts InventoryPartSupported { get { return mInventoryPart; } set { mInventoryPart = value; } }
+    }
+    [AttributeUsage(AttributeTargets.Interface)]
+    public class ImplAttr : Attribute
+    {
+        private Type[] mImplementorList;
+        public ImplAttr(Type[] Implementors) { mImplementorList = Implementors; }
+        public Type[] ImplementorList { get { return mImplementorList; } set { mImplementorList = value; } }
+    }
+    [InventoryPartAttribute(enmInvParts.Monitors)]
+    class MonitorInventory : IPartsInventory { public void Restock() { Console.WriteLine("monitor inventory restocked"); } }
+    [InventoryPartAttribute(enmInvParts.Keyboards)]
+    class KeyboardInventory : IPartsInventory { public void Restock() { Console.WriteLine("keyboard inventory restocked"); } }
+    [ImplAttr(new Type[] { typeof(MonitorInventory), typeof(KeyboardInventory) })]
+    interface IPartsInventory { public void Restock(); }
+    class PartsFactory
+    {
+        public IPartsInventory ReturnPartInventory(enmInvParts vInvPart)
+        {
+            IPartsInventory InvPart = null;
+            object Obj; Type[] IntrfaceImpl;
+            Attribute Attr;
+            enmInvParts enmInventoryPart;
+            InventoryPartAttribute InvPartAttr;
+            int ImplementorCount;
+            //Retrieve the attribute ImplAttr attached to the IPartsInventory 
+            //interface 
+            Attr = Attribute.GetCustomAttribute(typeof(IPartsInventory), typeof(ImplAttr));
+            //Retrieve the Type array containing the types that implement
+            //the IPartsInventory interface 
+            IntrfaceImpl = ((ImplAttr)Attr).ImplementorList;
+            //Determine the number of classes that 
+            //implement IPartsInventory 
+            ImplementorCount = IntrfaceImpl.GetLength(0);
+            for (int i = 0; i < ImplementorCount; i++)
+            {
+                Attr = Attribute.GetCustomAttribute(IntrfaceImpl[i], typeof(InventoryPartAttribute));
+                InvPartAttr = (InventoryPartAttribute)Attr;
+                //Determine what inventory part this class supports 
+                enmInventoryPart = InvPartAttr.InventoryPartSupported;
+                if ((int)enmInventoryPart == (int)vInvPart)
+                {
+                    Obj = Activator.CreateInstance(IntrfaceImpl[i]);
+                    InvPart = (IPartsInventory)Obj; break;
+                }
+            }
+            return InvPart;
+        }
+    }
+
+
+}
